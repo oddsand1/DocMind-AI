6aba296155aef45160645c992d3055c97ed07fa7
DocMind-AI文档智能分析助手 <2183231378@qq.com>
2026-04-25 17:58:32 +0800

    第一次提交

--- src/main/java/com/agent/ai_doc_agent/AiDocAgentApplication.java
+++ src/main/java/com/agent/ai_doc_agent/AiDocAgentApplication.java
@@ -3,9 +3,13 @@ package com.agent.ai_doc_agent;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 //改注解包含了@ComponentScan可以扫描启动类所在包及其子包下的spring组件，包括Controller、Service、Repository等
 @SpringBootApplication
+
+// 开启异步任务
+@EnableAsync
 
 // 扫描你mapper目录下的所有Mapper接口
 @MapperScan("com.agent.ai_doc_agent.mapper")
